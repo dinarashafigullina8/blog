e9c2ae58335f7e09ae5747963f4e32fd7b57ae94
Dinara <dineirashafigulllina@gmail.com>
2022-12-21 12:15:33 +0500

    made admin

--- blog/core/admin.py
+++ blog/core/admin.py
@@ -8,13 +8,15 @@ class CategoryAdmin(admin.ModelAdmin):
     search_fields = ('name',)
 
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('topic', 'text', 'date')
+    list_display = ('topic', 'text', 'date', 'image')
     list_display_links = ('topic',)
     search_fields = ('topic', 'date')
+    list_filter = ('topic', 'date')
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('post', 'first_name', 'last_name', 'gender', 'email', 'about', 'photo')
+    list_display = ('first_name', 'last_name', 'gender', 'email', 'about', 'photo')
     search_fields = ('first_name', 'last_name', 'gender', 'email')
+    list_filter = ('first_name', 'last_name', 'gender')
 
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post, PostAdmin)
--- blog/core/models.py
+++ blog/core/models.py
@@ -13,9 +13,32 @@ class Category(models.Model):
         ordering = ('name',)
 
 
+class User(models.Model):
+    man = 'М'
+    women = 'Ж'
+    gender_choices = [
+        (man, 'Мужчина'),
+        (women, 'Женщина')
+    ]
+    first_name = models.CharField(max_length = 255, verbose_name='Имя')
+    last_name = models.CharField(max_length = 255, verbose_name='Фамилия')
+    gender = models.CharField(max_length = 255, choices = gender_choices, verbose_name='Пол')
+    email = models.EmailField(verbose_name = 'Электронная почта')
+    about = models.TextField(verbose_name='О себе')
+    photo = models.ImageField(default='-', verbose_name='Фото')
+
+    # def __str__(self):
+    #     return self.name
+    
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('first_name', 'last_name')
+
 
 class Post(models.Model):
     categoryies = models.ManyToManyField(Category, related_name = 'category')
+    author = models.ForeignKey(User, on_delete = models.CASCADE,related_name = 'user', default=0)
     topic = models.CharField(max_length = 255, verbose_name = 'Тема')
     text = models.TextField(verbose_name = 'Пост')
     image = models.ImageField(default='-', verbose_name='Фото')
@@ -28,30 +51,3 @@ class Post(models.Model):
         verbose_name = 'Пост'
         verbose_name_plural = 'Посты'
         ordering = ('topic','date')
-
-
-
-class User(models.Model):
-    man = 'М'
-    women = 'Ж'
-    gender_choices = [
-        (man, 'Мужчина'),
-        (women, 'Женщина')
-    ]
-    post = models.ForeignKey(Post, on_delete = models.SET_NULL, null=True, related_name = 'author', default=0)
-    first_name = models.CharField(max_length = 255, verbose_name='Имя')
-    last_name = models.CharField(max_length = 255, verbose_name='Фамилия')
-    gender = models.CharField(max_length = 255, choices = gender_choices, verbose_name='Пол')
-    email = models.EmailField(verbose_name = 'Электронная почта')
-    about = models.TextField(verbose_name='О себе')
-    photo = models.ImageField(default='-', verbose_name='Фото')
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
-        ordering = ('first_name', 'last_name')
-
-
